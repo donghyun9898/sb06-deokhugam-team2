206b0689d0c1c06d7964edc7576e3a90503681fb
3Park <lovehyunju7@naver.com>
2025-12-02 12:03:28 +0900

    임시 수정 코드 원복
    임시 수정 코드가 올라가, 원복함.

--- src/main/java/com/codeit/sb06deokhugamteam2/comment/entity/Comment.java
+++ src/main/java/com/codeit/sb06deokhugamteam2/comment/entity/Comment.java
@@ -26,7 +26,7 @@ public class Comment {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @ManyToOne()
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "review_id", nullable = false)
     private Review review;
 
